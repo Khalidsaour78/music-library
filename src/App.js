0cf97a3560d99ea659da8a8a316486b4d27dd02b
Khalid <Alsaoork@gmail.com>
2024-05-10 11:05:52 +0300

    Adding a controlled form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-
 import { useEffect, useState } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
@@ -10,13 +7,31 @@ function App(){
     let [message, setMessage] = useState('Search for Music!')
     let [data, setData] = useState([])
 
-    return (
-        <div>
-            <SearchBar />
-            {message}
-            <Gallery />
-        </div>
-    )
+    useEffect(() => {
+        const fetchData = async () => {
+            document.title = `${search} Music`
+            const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
+            const resData = await response.json()
+            if (resData.results.length > 0) {
+              setData(resData.results)
+          } else {
+              setMessage('Not Found')
+          }
+      }
+      fetchData()
+  }, [search])
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    setSearch(term)
+}
+
+return (
+    <div>
+        <SearchBar handleSearch = {handleSearch} />
+        {message}
+        <Gallery />
+    </div>
+)
 }
 
 export default App
